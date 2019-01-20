8304f1c7b6fab4c6d7f4ce52b5628cef2db13174
Chiara97 <43986413+Chiara97@users.noreply.github.com>
2019-01-20 12:36:44 +0100

    Add files via upload

---
+++ UnitTesting_EmailCheck/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UnitTesting_EmailCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mailaddress = "ich@providercom";
+            testen(mailaddress);
+        }
+        public static bool IsEmailAddress(string emailAddress)
+        {
+            int iAt = emailAddress.IndexOf('@');
+            int iDot = emailAddress.LastIndexOf('.');
+            return (iAt > 0 && iDot > iAt); 
+        }
+        public static void testen(string emailAddress)
+        {
+            if (IsEmailAddress(emailAddress))
+            {
+                Console.WriteLine("TEST PASSED: " + emailAddress + " korrekt als Email-Adresse erkannt");
+            }
+            
+            else
+            {
+                Console.WriteLine("TEST FAILED: " + emailAddress + " nicht als Email-Adresse erkannt, obwohl korrekt.");
+            }
+        }
+    }
+}
